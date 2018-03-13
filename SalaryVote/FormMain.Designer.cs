4c051d957701e69ea3adc22279baa004e2f97c5c
DerSkythe <skif@skif.ws>
2018-03-13 11:02:00 +0400

    Add project files.

---
+++ SalaryVote/FormMain.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace SalaryVote
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.btnYes = new System.Windows.Forms.Button();
+            this.btnNo = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox.Image")));
+            this.pictureBox.Location = new System.Drawing.Point(231, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(163, 323);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblQuestion.Location = new System.Drawing.Point(138, 385);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(362, 25);
+            this.lblQuestion.TabIndex = 1;
+            this.lblQuestion.Text = "Довольны ли вы своей зарплатой?";
+            // 
+            // btnYes
+            // 
+            this.btnYes.Location = new System.Drawing.Point(200, 498);
+            this.btnYes.Name = "btnYes";
+            this.btnYes.Size = new System.Drawing.Size(75, 23);
+            this.btnYes.TabIndex = 2;
+            this.btnYes.Text = "Да";
+            this.btnYes.UseVisualStyleBackColor = true;
+            this.btnYes.Click += new System.EventHandler(this.btnYes_Click);
+            // 
+            // btnNo
+            // 
+            this.btnNo.Location = new System.Drawing.Point(356, 498);
+            this.btnNo.Name = "btnNo";
+            this.btnNo.Size = new System.Drawing.Size(75, 23);
+            this.btnNo.TabIndex = 3;
+            this.btnNo.TabStop = false;
+            this.btnNo.Text = "Нет";
+            this.btnNo.UseVisualStyleBackColor = true;
+            this.btnNo.Click += new System.EventHandler(this.btnNo_Click);
+            this.btnNo.MouseEnter += new System.EventHandler(this.btnNo_MouseEnter);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblResult.Location = new System.Drawing.Point(95, 385);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(436, 25);
+            this.lblResult.TabIndex = 4;
+            this.lblResult.Text = "Мы и не сомневались, что вы так думаете";
+            this.lblResult.Visible = false;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(647, 533);
+            this.Controls.Add(this.btnNo);
+            this.Controls.Add(this.btnYes);
+            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.lblResult);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Опросник";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Button btnYes;
+        private System.Windows.Forms.Button btnNo;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ SalaryVote/FormMain.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SalaryVote
+{
+    public partial class FormMain : Form
+    {
+        private bool _Answered;
+        private Rectangle _Rectangle;
+        private Random _Rnd;
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!_Answered)
+            {
+                e.Cancel = true;
+                MessageBox.Show(
+                                @"Пожалуйста, выберите ответ Да или Нет",
+                                @"Ответ",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Exclamation);
+            }
+        }       
+
+        private void btnNo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                            @"Этого не должно было произойти!",
+                            @"Ответ",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Error);
+        }
+
+        private void btnYes_Click(object sender, EventArgs e)
+        {
+            lblResult.Visible = _Answered = true;
+            lblQuestion.Visible = btnYes.Visible = btnNo.Visible = false;
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            _Rectangle = new Rectangle(btnNo.Location, btnNo.Size);
+            _Rnd = new Random((int)DateTime.Now.Ticks);
+        }
+
+        private void NewLocation(Point cursorLocation)
+        {
+            do
+            {
+                _Rectangle.Location = new Point(
+                                                _Rnd.Next(0, ClientSize.Width - _Rectangle.Width),
+                                                _Rnd.Next(0, ClientSize.Height - _Rectangle.Height));
+            } while (CheckCursor(cursorLocation));
+
+            btnNo.Location = _Rectangle.Location;
+        }
+
+        private bool CheckCursor(Point cursorLocation)
+        {
+            return (cursorLocation.Y >= _Rectangle.Bottom && cursorLocation.Y <= _Rectangle.Top) ^
+                   (cursorLocation.X <= _Rectangle.Right && cursorLocation.X >= _Rectangle.Left);
+        }
+
+        private void btnNo_MouseEnter(object sender, EventArgs e)
+        {
+            NewLocation(Cursor.Position);
+        }
+    }
+}
